5a03cda4ae9038669b845ef98096fb220303d2c2
usuario ftp <uftp@debian8.(none)>
2015-04-10 09:45:41 -0300

    saving latest changes to the repo

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function ($) {
+  $(document).ready(function() {
+    $(".main-menu").naver({
+      maxWidth: "740px",
+    });
+    $.scrollUp({
+        scrollText: ''// Text for element
+      });
+  });
+})(jQuery);
